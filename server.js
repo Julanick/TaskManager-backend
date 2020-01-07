eaa8176ff32c63da89ff2b9386f06279b850fb40
Julanick <julanick@gmail.com>
2020-01-07 15:47:15 +0300

    fix: fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, { useUnifiedTopology: true }, (err, database) => {
     if (err) return console.log(err)
